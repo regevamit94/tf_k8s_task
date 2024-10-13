4a639a183d6e52669790d846a30ae5d321dff307
regevamit94 <ubuntu@ip-172-26-12-16.eu-central-1.compute.internal>
2024-10-13 22:34:23 +0000

    First step

---
+++ main.tf
@@ -0,0 +1,92 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+
+    subscription_id = var.subscription_id
+    client_id = var.client_id
+    client_secret = var.client_secret
+    tenant_id = var.tenant_id
+
+    resource_provider_registrations = "none"
+   }
+
+
+resource "azurerm_resource_group" "hometask_RG" {
+  name     = var.my_resource_group_name
+  location = var.region
+
+  tags = {
+    Name = "hometask resource group"
+  }
+
+}
+
+resource "azurerm_virtual_network" "hometask_virtual_network" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = var.region
+  resource_group_name = azurerm_resource_group.hometask_RG.name
+
+  tags = {
+    Name = "hometask virtual network"
+  }
+}
+
+resource "azurerm_subnet" "hometask_subnet" {
+  name = var.subnet_name
+  resource_group_name = azurerm_resource_group.hometask_RG.name
+  virtual_network_name = azurerm_virtual_network.hometask_virtual_network.name
+  address_prefixes = var.subnet_cidr
+} 
+
+resource "azurerm_public_ip" "hometask_public_ip" {
+  name                = "hometask-public-ip"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.hometask_RG.name
+  allocation_method   = "Static"
+
+tags = {
+    Name = "hometask public ip"
+  }
+}
+
+
+resource "azurerm_kubernetes_cluster" "hometask_AKS" {
+name                = "hometask_AKS_Cluster"
+resource_group_name = azurerm_resource_group.hometask_RG.name
+location            = azurerm_resource_group.hometask_RG.location
+dns_prefix          = "hometaskK8S"
+
+default_node_pool {
+    name       = "agentpool"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.hometask_subnet.id
+    }
+
+identity {
+    type = "SystemAssigned"
+    }
+
+network_profile {
+    network_plugin    = "azure"
+    dns_service_ip    = "10.0.0.2"
+    service_cidr      = "10.0.0.0/24"
+    }
+
+    tags = {
+        Name = "hometask Azure K8S Cluster"
+    }
+}
+
---
+++ variables.tf
@@ -0,0 +1,60 @@
+########### Start of privileged data #############
+variable "subscription_id" {
+  type        = string
+  description = "My Azure account subscription ID"   
+}
+
+variable "client_id" {
+  type        = string
+  description = "My Azure account client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "My Azure account client cecret"
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "My Azure account tenant ID"
+}
+########### End of privileged data #############
+
+variable "region" {
+  type        = string
+  default     = "West Europe"
+  description = "The region I use"
+}
+
+variable "my_resource_group_name" {
+  type        = string
+  default     = "hometask_RG"
+  description = "My resource group name"
+}
+
+variable "vnet_name" {
+  type        = string
+  default     = "hometask_VNet"
+  description = "My virtual network name"
+}
+
+variable "address_space" {
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+  description = "My subnet"
+}
+
+variable "subnet_name" {
+  type        = string
+  default     = "hometask_Subnet"
+  description = "My subnet name"
+}
+
+variable "subnet_cidr" {
+  type        = list(string)
+  default     = ["10.0.0.0/24"]
+  description = "My subnet cidr"
+}
+
+
